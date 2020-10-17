964229b6c1c610555b0035fd5608c607325ac855
Ilya Razenshteyn <ilya.razenshteyn@gmail.com>
2020-10-17 08:02:43 -0700

    Update weight

--- weight/process.py
+++ weight/process.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+from matplotlib import pyplot as plt
 
 
 with open('weight.csv', 'r') as f:
@@ -10,4 +11,24 @@ with open('weight.csv', 'r') as f:
         d = datetime.datetime.strptime(row[0], '%m/%d/%Y')
         w = float(row[1])
         data.append((d, w))
-    print(data)
+    plt.ylim((73, 118))
+    plt.xticks(rotation='vertical')
+    plt.subplots_adjust(bottom=0.17)
+    plt.grid(True)
+    for window in [1, 7]:
+        if window % 2 == 0:
+            raise Exception('Even window size: median is ambiguous')
+        xaxis = []
+        yaxis = []
+        for i in range(len(data)):
+            if i < window - 1:
+                continue
+            xaxis.append(data[i][0])
+            cur_window = []
+            for j in range(window):
+                cur_window.append(data[i - j][1])
+            cur_window.sort()
+            med = cur_window[window // 2]
+            yaxis.append(med)
+        plt.plot(xaxis, yaxis)
+    plt.savefig('weight.png')
--- weight/sync.py
+++ weight/sync.py
@@ -5,5 +5,6 @@ def run(cmd):
         raise Exception('Failed to run: \'{}\''.format(cmd))
 
 run('git add weight.csv')
+run('git add weight.png')
 run('git commit -m "Update weight"')
 run('git push')
