e40ded552a7dea565377804f7aaca23c65fe476d
Ilya Razenshteyn <ilya.razenshteyn@gmail.com>
2020-10-18 09:06:21 -0700

    Update the exercise sync script

--- exercise/sync.py
+++ exercise/sync.py
@@ -4,6 +4,6 @@ def run(cmd):
     if os.system(cmd) != 0:
         raise Exception('Failed to run: \'{}\''.format(cmd))
 
-run('git add exercise.csv')
+run('git add wahoo')
 run('git commit -m "Update exercise"')
 run('git push')
