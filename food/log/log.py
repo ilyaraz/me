7801c32cbb6aa874356c7017061f80a0ab0df813
Ilya Razenshteyn <ilya.razenshteyn@gmail.com>
2020-10-20 12:56:07 -0700

    Update food

---
+++ food/log/log.py
@@ -0,0 +1,55 @@
+import pint
+import datetime
+from dataclasses import dataclass
+
+ur = pint.UnitRegistry()
+
+def cup(x):
+    return ur.cup * x
+
+def ct(x):
+    return ur.count * x
+
+def gr(x):
+    return ur.gram * x
+
+def dt(x):
+    return datetime.datetime.strptime(x, '%Y-%m-%d')
+
+@dataclass
+class Food:
+    name: str
+    calories: float
+    amount: pint.Quantity
+
+def count_calories(food):
+    for day in sorted(food.keys()):
+        print(day.date())
+        total = 0.0
+        for (food_item, amount) in food[day]:
+            calories = food_item.calories * amount / food_item.amount
+            print(food_item.name, float(calories))
+            total += calories
+        print('Total: {}'.format(float(total)))
+
+egg = Food('egg', 80, ct(1))
+bobs_red_mill_old_fashioned_rolled_oats = Food('oatmeal', 180, cup(0.5))
+chicken_breast = Food('chicken breast', 165, gr(100))
+kirkland_cookies_cream_protein_bar = Food('protein bar', 180, ct(1))
+green_lentils = Food('green lentils', 145, cup(0.25))
+
+food = {}
+
+oatmeal = bobs_red_mill_old_fashioned_rolled_oats
+protein_bar = kirkland_cookies_cream_protein_bar
+lentils = green_lentils
+
+food[dt('2020-10-20')] = [
+    (oatmeal, cup(0.75)),
+    (egg, ct(4)),
+    (chicken_breast, gr(127)),
+    (protein_bar, ct(1)),
+    (lentils, cup(0.75))
+]
+
+count_calories(food)
