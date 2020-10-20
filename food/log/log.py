60893c85bb3df944ae26ec1b3bf4ab46289188ba
Ilya Razenshteyn <ilya.razenshteyn@gmail.com>
2020-10-20 14:33:30 -0700

    Update food

--- food/log/log.py
+++ food/log/log.py
@@ -13,6 +13,12 @@ def ct(x):
 def gr(x):
     return ur.gram * x
 
+def tbsp(x):
+    return ur.tablespoon * x
+
+def tsp(x):
+    return ur.teaspoon * x
+
 def dt(x):
     return datetime.datetime.strptime(x, '%Y-%m-%d')
 
@@ -37,19 +43,30 @@ bobs_red_mill_old_fashioned_rolled_oats = Food('oatmeal', 180, cup(0.5))
 chicken_breast = Food('chicken breast', 165, gr(100))
 kirkland_cookies_cream_protein_bar = Food('protein bar', 180, ct(1))
 green_lentils = Food('green lentils', 145, cup(0.25))
+frozen_strawberries = Food('frozen strawberries', 50, gr(140))
+pb2 = Food('PB2', 60, tbsp(2))
+protein_powder_whey_isolate = Food('protein powder', 140, gr(39))
+xanthan_gum = Food('xanthan gum', 5, tsp(0.5))
+cashew_milk = Food('cashew milk', 25, cup(1))
 
 food = {}
 
 oatmeal = bobs_red_mill_old_fashioned_rolled_oats
 protein_bar = kirkland_cookies_cream_protein_bar
 lentils = green_lentils
+protein_powder = protein_powder_whey_isolate
 
 food[dt('2020-10-20')] = [
     (oatmeal, cup(0.75)),
     (egg, ct(4)),
     (chicken_breast, gr(127)),
     (protein_bar, ct(1)),
-    (lentils, cup(0.75))
+    (lentils, cup(0.75)),
+    (frozen_strawberries, gr(130)),
+    (pb2, tbsp(2)),
+    (protein_powder, gr(39)),
+    (xanthan_gum, tsp(0.5)),
+    (cashew_milk, cup(0.5))
 ]
 
 count_calories(food)
