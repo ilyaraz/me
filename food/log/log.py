738b2c60d69f45cb008013cc727d3627a1d265e4
Ilya Razenshteyn <ilya.razenshteyn@gmail.com>
2020-10-24 19:49:58 -0700

    Update food

--- food/log/log.py
+++ food/log/log.py
@@ -170,7 +170,8 @@ food[dt('2020-10-24')] = [
     (pb2, tbsp(4)),
     (protein_powder, gr(78)),
     (xanthan_gum, tsp(1.0)),
-    (cashew_milk, cup(1.0))
+    (cashew_milk, cup(1.0)),
+    (banana, gr(106))
 ]
 
 count_calories(food)
