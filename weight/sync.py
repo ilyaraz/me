b5f7a72f3449f845ac1af3ac3161ee7699dbd8ff
Ilya Razenshteyn <ilya.razenshteyn@gmail.com>
2020-11-11 16:13:02 -0800

    Sync blood together with weight

--- weight/sync.py
+++ weight/sync.py
@@ -5,6 +5,7 @@ def run(cmd):
         raise Exception('Failed to run: \'{}\''.format(cmd))
 
 run('git add weight.csv')
+run('git add blood.csv')
 run('git add weight.png')
 run('git commit -m "Update weight"')
 run('git push')
